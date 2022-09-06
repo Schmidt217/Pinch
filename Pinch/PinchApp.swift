5ed7172e8c6c30d1c2eaed2f765bdd66bc3ebee8
Mike Schmidt <schmidt217@gmail.com>
2022-09-06 11:29:37 -0600

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Michael Schmidt on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
